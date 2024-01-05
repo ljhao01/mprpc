6a8fda4ca732d96841eb98086e79558b3f01203e
ljhao01 <1486840062@qq.com>
2024-01-05 18:15:06 +0800

    rpc

--- src/include/mprpcapplication.h
+++ src/include/mprpcapplication.h
@@ -1,1 +1,11 @@
 #pragma once
+#include "google/protobuf/service.h"
+
+//mprpc框架的基础类，负责框架的一些初始化操作，使用单例模式
+class MprpcApplication {
+public:
+    static void Init(int argc, char** argv);
+    static MprpcApplication& GetInstance();
+private:
+
+};
