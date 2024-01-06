eb9c51a575958ccdaccc31850b67b7b095faba74
ljhao01 <1486840062@qq.com>
2024-01-06 15:27:37 +0800

    rpc

---
+++ build/src/CMakeFiles/mprpc.dir/mprpcapplication.cc.o.d
@@ -0,0 +1,2 @@
+src/CMakeFiles/mprpc.dir/mprpcapplication.cc.o: \
+ /home/ljh/mprpc/src/mprpcapplication.cc /usr/include/stdc-predef.h
---
+++ build/src/CMakeFiles/mprpc.dir/rpcprovider.cc.o.d
@@ -0,0 +1,2 @@
+src/CMakeFiles/mprpc.dir/rpcprovider.cc.o: \
+ /home/ljh/mprpc/src/rpcprovider.cc /usr/include/stdc-predef.h
